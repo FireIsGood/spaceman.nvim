c03eaecef53a312f9cd1f4ad0b3ec9763788e4b7
FireIsGood <fireisadequate@gmail.com>
2024-05-27 15:52:47 -0700

    refactor: local default command variable

--- lua/spaceman/default_commands.lua
+++ lua/spaceman/default_commands.lua
@@ -4,13 +4,13 @@ local cmd = vim.api.nvim_create_user_command
 
 --------------------------------------------------------------------------------
 
-local default_keymaps = {
+local default_user_commands = {
   { command = "list_workspaces", user_cmd = "Spaceman", description = "List workspaces" },
 }
 
 -- User commands
 function M.setup()
-  for _, binding in pairs(default_keymaps) do
+  for _, binding in pairs(default_user_commands) do
     cmd(binding.user_cmd, function()
       require("spaceman")[binding.command]()
     end, { desc = binding.description })
