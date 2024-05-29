8af1e915780dd0bdf4a787869c783b02ab2801b3
FireIsGood <fireisadequate@gmail.com>
2024-05-29 12:35:55 -0700

    chore: clean up accidental file

--- test.lua
+++
@@ -1,8 +0,0 @@
-require("spaceman").setup({
-  -- [OTHER SETTINGS]
-  telescope_opts = require("telescope.themes").get_dropdown({
-    prompt_title = "Cool Dropdown",
-    results_title = "Items or Something",
-    scroll_strategy = "limit",
-  }),
-})
